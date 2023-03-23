e285ff01328e01617065a9f3fc24a038d9df17ec
Uditi1 <udititiwari@gmail.com>
2023-03-23 22:51:31 +0530

    morecode

--- app/page.js
+++ app/page.js
@@ -1,16 +1,37 @@
-import { Footer, Navbar } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+import { Footer, Navbar } from "../components";
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from "../sections";
 
 const Page = () => (
-  <div className='bg-primary-black overflow-hidden'>
+  <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
-    <Explore />
-    <GetStarted />
-    <WhatsNew />
-    <World />
-    <Insights />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+    </div>
+
+    <div className="relative">
+      <GetStarted />
+      <div className="gradient-03 z-0" />
+      <WhatsNew />
+    </div>
+
+    <div className="relative">
+      <World />
+      <div className="gradient-03 z-0" />
+      <Insights />
+    </div>
+
     <Feedback />
     <Footer />
   </div>
